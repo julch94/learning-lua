636e04ec841e981abc252a97b2089308be92be45
julch94 <julch94@gmail.com>
2023-06-20 22:58:50 -0500

    Added Module 1 assignments into a Module 1 folder

---
+++ Module1/module-1-4-single-line-comments.lua
@@ -0,0 +1,7 @@
+-- Start the program by greeting the world! Prints "hi"
+print ("hi")
+
+print("hello") -- Prints "hello"
+
+-- 😊
+print("Codecademy")
---
+++ Module1/module-1-6-letter-to-self.lua
@@ -0,0 +1,16 @@
+-- module-1-6-letter-to-self
+-- Letter To Self
+--[[
+This should not show
+--]]
+print("Dear Self,")
+print()
+print("My primary goals from this course are:")
+print()
+print("1. Gain a basic understanding of how Lua works and its place among other programming languages.")
+print("2. Learn how to use Lua to solve simple scripting problems.")
+print("3. Get my feet wet in a dynamically typed language as preparation for the free MIT CS course with Python.")
+print ("4. Begin to understand how Lua is used in the Game Industry.")
+print()
+print("Nick Julch")
+print("20 June 2023")
