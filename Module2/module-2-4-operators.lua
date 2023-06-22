0b82edb3bd836d8503700e9b912b8ff706e61260
julch94 <julch94@gmail.com>
2023-06-21 20:49:24 -0500

    added 2-4

---
+++ Module2/module-2-4-operators.lua
@@ -0,0 +1,36 @@
+-- Module 2-4 Assignment
+-- Using Operators
+
+print("Ji-Eun's current cart total:")
+-- Discover and print Ji-Eun's current total below using the addition operator
+cartItem1 = 10.95
+cartItem2 = 20.99
+print(cartItem1 + cartItem2)
+
+
+print("Luca's ice cream inventory:")
+-- Discover and print Luca's ice cream inventory below using the subtraction operator
+conesInit = 300
+conesSold = 298
+print(conesInit - conesSold)
+
+
+print("Today's Net Total for Billy's Hat Store:")
+-- Discover and print Billy's revenue below using the multiplication operator
+cowboyHatPrice = 60
+cowboyHatsSold = 49
+print(cowboyHatPrice * cowboyHatsSold)
+
+
+print("The cost of one of Sage's pizza: ")
+-- Discover and print the cost of one pizza below using the division operator
+pizzasBought = 8
+dollarsSpent = 96
+print(dollarsSpent / pizzasBought)
+
+
+print("Marbles leftover for Layla: ")
+-- Discover and print the remainder marble below using the modulo operator
+numMarbles = 7
+numPeople = 2
+print(numMarbles % numPeople)
