bcfb84586e23d5695212d8ae15bdb4bf1b7e3e8f
julch94 <julch94@gmail.com>
2023-06-21 20:21:23 -0500

    Adding module 2 folder - module2-2 and module2-3 scripts done so far

---
+++ Module2/module-2-2-simple-data-type-printing.lua
@@ -0,0 +1,5 @@
+-- Module 2-2 Assignment - Basic Data Types
+print(1) -- Number of languages I can speak
+print("The Prestige") -- My favorite movie
+print(true) -- Did I eat breakfast?
+print(nil) -- nil
---
+++ Module2/module-2-3-using-type-function.lua
@@ -0,0 +1,6 @@
+-- Module 2-3 Assignment
+-- 
+print(type(5)) -- I have picked the number 5 and represented it as a number. This should return "number"
+print(type("5")) -- By adding quotes around the number, it should now return "string"
+print(type(true)) -- This will return boolean as the type
+print(type("true")) -- By adding quotes around true, this is now a string
