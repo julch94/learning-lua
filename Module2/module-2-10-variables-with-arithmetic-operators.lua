3fbae3895a77a9aa2b015a029afafa8d6ad909ec
julch94 <julch94@gmail.com>
2023-06-21 22:41:22 -0500

    added 2-10 assignment

---
+++ Module2/module-2-10-variables-with-arithmetic-operators.lua
@@ -0,0 +1,18 @@
+-- Module 2-10 Assignment
+-- Variables with arithmetic operators
+
+-- Initial values, don't change these!
+player1Score = 10
+player2Score = 10
+
+-- Write your code below:
+-- Kamala +1 point
+player1Score = player1Score + 1
+print(player1Score)
+-- Bruno -2 points
+player2Score = player2Score - 2
+print(player2Score)
+
+-- Bruno 1.5x multiplier
+player2Score = player2Score * 1.5
+print(player2Score)
