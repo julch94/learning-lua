7819cb061480f2435e9414b13e2e1bb91851a0a1
julch94 <julch94@gmail.com>
2023-06-21 22:33:54 -0500

    add 2-9 assignment

---
+++ Module2/module-2-9-variable-reassignment.lua
@@ -0,0 +1,16 @@
+-- Module 2-9 Assignment
+-- Variable Reassignment
+-- Initial values, don't change this!
+highestScore = 25
+
+-- Kamala initial high score
+highestScorerName = "Kamala"
+print(highestScorerName)
+
+-- Bruno takes the lead
+highestScorerName = "Bruno"
+highestScore = 26
+
+-- Print latest high score and name
+print(highestScorerName)
+print(highestScore)
