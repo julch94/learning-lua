052d9b0a4dc87aa30306bb07c0bc26425243fe17
julch94 <julch94@gmail.com>
2023-06-21 22:52:00 -0500

    add 2-11 assignment

---
+++ Module2/module-2-11-concatenation.lua
@@ -0,0 +1,10 @@
+-- Module 2-11
+-- Concatenation
+-- Initial values, do not make your changes here!
+player1Name = "Kamala"
+player2Name = "Bruno"
+
+-- Write your code below: 
+print("The first player is: " .. player1Name)
+player2Name = "Nakia"
+print("The second player is: " .. player2Name)
