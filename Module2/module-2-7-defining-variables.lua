e93cc25f961c812876daab8eaff8f1457b1678c9
julch94 <julch94@gmail.com>
2023-06-21 22:18:32 -0500

    added 2-7 and 2-8 assignments

---
+++ Module2/module-2-7-defining-variables.lua
@@ -0,0 +1,4 @@
+-- Module 2-7 Assignment
+player1Name = "Kamala"
+player2Name = "Bruno"
+highestScore = nil
---
+++ Module2/module-2-8-using-type-with-variables.lua
@@ -0,0 +1,7 @@
+-- Module 2-8 Assignment
+-- Using type() with variables
+
+userName = "julch94"
+print(type(userName))
+age = 28
+print(type(age))
