2239d9a957e97e06e469c1e5f7556b9069ff62e3
unknown <julch94@gmail.com>
2023-06-23 18:37:45 -0700

    Added module 3 and module 3-2 assignment

---
+++ Module3/module-3-2-conditionals-and-logic.lua
@@ -0,0 +1,11 @@
+-- Module 3-2 Assignment
+-- module-3-2-conditionals-and-logic.lua
+
+isSunny = false
+print("What is the weather like?")
+
+--Add your conditional statment here
+if isSunny then
+  print("It is sunny outside!")
+end
+print("Good to know! Thanks!")
