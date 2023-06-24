4f47ed6d50191bdd2bf7fc1cb97783908c475ec2
unknown <julch94@gmail.com>
2023-06-23 19:31:38 -0700

    Added 3-5 assignment

---
+++ Module3/module-3-5-elseif-statement.lua
@@ -0,0 +1,62 @@
+-- Module 3-5 Assignment
+-- module-3-5-elseif-statement.lua
+
+-- Original value
+score = 90
+print("Score = " .. score)
+print()
+if score == 100 then
+  print("Winner winner chicken dinner")
+elseif score > 80 then
+  print("Close but not close enough")
+elseif score >= 60 then
+  print("Try again")
+else
+  print("Were you even playing?")
+end
+print()
+
+-- Perfect score
+score = 100
+print("Score = " .. score)
+print()
+if score == 100 then
+  print("Winner winner chicken dinner")
+elseif score > 80 then
+  print("Close but not close enough")
+elseif score >= 60 then
+  print("Try again")
+else
+  print("Were you even playing?")
+end
+print()
+
+-- Low score
+score = 55
+print("Score = " .. score)
+print()
+if score == 100 then
+  print("Winner winner chicken dinner")
+elseif score > 80 then
+  print("Close but not close enough")
+elseif score >= 60 then
+  print("Try again")
+else
+  print("Were you even playing?")
+end
+print()
+
+-- Try Again score
+score = 65
+print("Score = " .. score)
+print()
+if score == 100 then
+  print("Winner winner chicken dinner")
+elseif score > 80 then
+  print("Close but not close enough")
+elseif score >= 60 then
+  print("Try again")
+else
+  print("Were you even playing?")
+end
+print()
