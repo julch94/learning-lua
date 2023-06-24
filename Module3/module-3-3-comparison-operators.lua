7b3a2514880e17fbfa6f58dd7a8489be67aa1f4d
unknown <julch94@gmail.com>
2023-06-23 18:57:14 -0700

    adding 3-3 assignment

---
+++ Module3/module-3-3-comparison-operators.lua
@@ -0,0 +1,47 @@
+-- Module 3-3 Assignment
+-- module-3-3-comparison-operators.lua
+
+-- Initial run from the exercise
+A = 5
+B = 10
+print("A = " .. A)
+print("B = " .. B)
+if A > B then
+  print("A is larger")
+end
+if B > A then
+  print("B is larger")
+end
+if A == B then
+  print("They are equal.")
+end
+
+-- Swapping A and B values
+A = 10
+B = 5
+print("A = " .. A)
+print("B = " .. B)
+if A > B then
+  print("A is larger")
+end
+if B > A then
+  print("B is larger")
+end
+if A == B then
+  print("They are equal.")
+end
+
+-- Setting A and B equal
+A = 4
+B = 4
+print("A = " .. A)
+print("B = " .. B)
+if A > B then
+  print("A is larger")
+end
+if B > A then
+  print("B is larger")
+end
+if A == B then
+  print("They are equal.")
+end
