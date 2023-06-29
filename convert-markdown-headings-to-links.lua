213d4db597bae50737e4b2b0674c6151293ded72
unknown <julch94@gmail.com>
2023-06-28 21:05:27 -0700

    updated TOC for lua-users-tutorial and finished first section

--- convert-markdown-headings-to-links.lua
+++ convert-markdown-headings-to-links.lua
@@ -3,77 +3,101 @@
 -- 26 June 2023
 
 --[[
-	This script was developed by outlining the desired functions to ChatGPT and instructing the tool to develop a script along with steps for creating said script
+	This script was developed by outlining the desired functions to ChatGPT and instructing the tool to develop a script along with steps for creating said script. This script was NOT directly created by ChatGPT, but instead was developed by following the tutorial mentioned above with my own interpretation
 	
-	This script is NOT directly created by ChatGPT, but instead was developed by following the tutorial mentioned above with my own interpretation
+	This script is also to supposed to serve as a guide for the many pieces within the script as I am still learning Lua and have yet to done any file I/O or major script manipulation.
 	
-	This script is also to supposed to serve as a guide for the many pieces within the script as I am still learning Lua and have yet to done any file I/O or major script manipulation
-	
-	To use this script: replace "MyFile.md" with the file path for the desired Markdown file to be processed and then run this script. It will print the results to the terminal. To save the results to a file, create an empty .txt file in this same directory called "HeadingLinks.txt"
-	
-	For reference, the ChatGPT conversation mentioned above can be found at: https://chat.openai.com/share/b6457f26-a922-42a3-852d-97935e8b099a
+	To use this script: replace inputFilename and outputFilename with the file path for the desired Markdown file to be processed and txt file to be output to and then run this script. It will print the results to the terminal. NOTE: the output file will be created if it doesn't exist.
 --]]
 
--- Read in the Markdown file
-local markdownFile = io.open("README.md", "r") -- io.open function opens the file in read mode
 
--- Extract the Headings
-local headings = {}
+local inputFilename = "lua-users-tutorial.md"  -- Replace with your Markdown file's name
+local outputFilename = "table_of_contents.txt"  -- Replace with the desired output file name
 
-for line in markdownFile:lines() do -- iterate through each line of the markdown file
-	local heading = line:match("^%s*#+%s*(.+)") -- regular expression pattern for a heading - see more on this below
-	if heading then -- if it matches our regex, 
-		table.insert(headings, heading) -- add to our table of headings
-	end
+
+local function collectHeadings(filename)
+  local file = io.open(filename, "r")
+  if not file then
+    return nil
+  end
+
+  local headings = {}
+  local currentHeading = nil
+
+  for line in file:lines() do
+    local hashCount, heading = line:match("^(#+)%s+(.+)$")
+    if hashCount and heading then
+      local headingLevel = string.len(hashCount)
+      if headingLevel == 2 or headingLevel == 3 then
+        local formattedHeading = string.rep("  ", headingLevel - 2) .. " [" .. heading .. "](#" .. string.lower(heading:gsub("%s+", "-")) .. ")"
+        table.insert(headings, formattedHeading)
+      end
+    end
+  end
+
+  file:close()
+  return headings
 end
 
-markdownFile:close() -- close the file
 
-local links = {}
--- Convert the Headings to Links
-for i, heading in ipairs(headings) do
-	local linkText = "[" .. heading .. "](#" .. heading:lower():gsub("%s", "-") .. ")"
-	table.insert(links, linkText)
-end
--- first we use [ heading ] to add the actual heading in brackets which will be the clickable text in the link
--- then we print that string again except all lower case, with spaces replaced by hyphens, and a # in front of the string and all thrown in parentheses to act as the link
-
--- Print each link along with its index
-for i, link in ipairs(links) do -- iterates through the whole links table
-	print("Link " .. i .. ": " .. link) -- for each link, print index and link text
+local function printTableOfContents(headings)
+  print("Table of Contents:")
+  for _, heading in ipairs(headings) do
+    print(heading)
+  end
 end
 
--- Save table to file
-local linksFileName = "HeadingLinks.txt" -- variable for our destination file name
-local linksFile = io.open(linksFileName, "w") -- open the file in write mode
-if not linksFile then
-	error("ERROR - COULD NOT OPEN FILE: " .. linksFileName) -- error check
-	return
+local function saveTableOfContentsToFile(headings, outputFilename)
+  local file = io.open(outputFilename, "w")
+  if not file then
+    print("Failed to open the file for writing.")
+    return
+  end
+
+  file:write("Table of Contents:\n")
+  for _, heading in ipairs(headings) do
+    file:write(heading .. "\n")
+  end
+
+  file:close()
+  print("Table of Contents saved to:", outputFilename)
 end
 
-for _, link in ipairs(links) do
-	linksFile:write(link .. "\n") -- add a line break between links and write each link
+local headings = collectHeadings(inputFilename)
+if headings then
+  printTableOfContents(headings)
+  saveTableOfContentsToFile(headings, outputFilename)
+else
+  print("Failed to open the file:", inputFilename)
 end
-
-linksFile:close() -- close the file
 
 --[[
-Additional information on the regex for markdown headings:
 
-^  - the start of the string
-%s* - zero or more whitespace characters
-#+ - one or more # characters, which are found before the text of a heading
-%s* - same as previous %s*
-(.+) - one or more characters after the whitespace and # characters - this is the actual heading text.
+Collecting Headings
+- The `collectHeadings` function reads the Markdown file line by line and extracts the headings.
+- The function opens the file using `io.open` with the provided filename in read mode (`"r"`).
+- It then initializes an empty table called `headings` to store the collected headings.
+- The `for` loop iterates over each line in the file using `file:lines()`.
+- The line is matched against the pattern `^(#+)%s*(.+)$`, which captures the hash symbols (`#+`) and the heading text (`.+`).
+- If a match is found and both the hash count and heading text are present, the headingLevel of the heading is determined by the length of the hash count.
+- If the headingLevel is 2 or 3 (indicating a two or three hash count), the heading is formatted as a Markdown link using the `string.rep` function to add indentation based on the heading headingLevel.
+- The formatted heading is then inserted into the `headings` table using `table.insert`.
 
-The regex pattern matches any lines that contain optional white space followed by at least one #, more optional white space, and then text (heading content).
---]]
+Printing the Table of Contents
+- The `printTableOfContents` function takes the `headings` table as input and prints the Table of Contents to the console.
+- It iterates over each heading in the `headings` table using `ipairs`.
+- The `print` function is used to display each heading.
 
---[[
-	Lesson Learned from this exercise:
-	1. More learning required on tables
-	2. More learning required on file I/O
-	3. More learning required on for loops in lua
-	4. More learning required on regex in lua and in general
-	5. More learning required on ipairs and other pattern tools
+Saving the Table of Contents to a File
+- The `saveTableOfContentsToFile` function takes the `headings` table and an output filename as input.
+- It opens the specified file in write mode (`"w"`) using `io.open`.
+- If the file fails to open, it displays an error message and returns.
+- It writes the Table of Contents header and each heading to the file using the `file:write` function.
+- Finally, it closes the file and displays a success message.
+
+Running the Script
+- The script calls the `collectHeadings` function to collect the headings from the input Markdown file.
+- If the headings are successfully collected, it calls the `printTableOfContents` function to display the Table of Contents on the console.
+- It then prompts the user to enter the desired output filename and calls the `saveTableOfContentsToFile` function to save the Table of Contents to a file.
+
 --]]
--- lua-users-tutorial.md
+++ lua-users-tutorial.md
@@ -1,3 +1,133 @@
 # lua-users-tutorial
 
-Placeholder for notes and scripts while working through the Lua-Users Tutorail
+Placeholder for notes and scripts while working through the Lua-Users Tutorial
+
+# Table of Contents
+
+- [Lua Types](#lua-types)
+- [Assignment](#assignment)
+- [Numbers](#numbers)
+- [Strings](#strings)
+- [Operators](#operators)
+- [Control Structure](#control-structure)
+- [Tables](#tables)
+- [Functions](#functions)
+- [Scope](#scope)
+- [Metamethods](#metamethods)
+- [Environments](#environments)
+- [Modules](#modules)
+- [Object Orientation](#object-orientation)
+- [Patterns](#patterns)
+- [Coroutines](#coroutines)
+- [Iterators](#iterators)
+- [Garbage Collection](#garbage-collection)
+- [Weak Tables](#weak-tables)
+- [Lua Standard Libraries](#lua-standard-libraries)
+  - [Core Functions](#core-functions)
+  - [Math Library](#math-library)
+  - [String Library](#string-library)
+  - [Table Library](#table-library)
+  - [OS Library](#os-library)
+  - [IO Library](#io-library)
+  - [Module Library](#module-library)
+  - [Debug Library](#debug-library)
+  - [Coroutine Library](#coroutine-library)
+- [For](#for)
+- [Advanced Concepts](#advanced-concepts)
+  - [Threads](#threads)
+  - [Optimisation](#optimisation)
+  - [Inheritance](#inheritance)
+
+## Lua Types
+
+Lua types include:
+	* Numbers - myNumber = 2
+	* Strings - myString = "hello"
+	* Boolean - myBool = true
+	* Tables - myTable = {}
+	* Functions - myFunction() end
+	* nil values - myNil = nil
+	* Userdata - see other section
+	* Thread - see other section
+
+My previous studying of the Lua language had plenty of detail on all of the Lua types outside of Tables, Userdata, and Thread. 
+
+There is a specific tutorial for tables so this section doesn't have a ton of information. Tables basically make up any type of collection that you would normally see represented with several types in other languages. Tables are created using a pair of curly bracket like so:
+
+~~~
+myTable = {}
+~~~
+
+There is a specific tutorial for userdata so this section doesn't have a ton of information. Userdata is described as foreign objects, such as those implemented from C and C++ programs. The summary here is that Lua can simply pass those values around but not actually change them. Metamethods (explained later) can be used to have userdata act similarly to tables.
+
+The Coroutines tutorial goes into detail on threads, however, they are summarized in this section as representations of independent (cooperative) threads of execution.
+
+This section also goes over Lua having dynamic typing (types are checked while a program is running).
+
+The last part of this section is regarding being able to query the type of a particular object. For example, if x = "dog" and we type print(x, type(x)), this will print
+
+	dog   string
+
+## Assignment
+
+## Numbers
+
+## Strings
+
+## Operators
+
+## Control Structure
+
+## Tables
+
+## Functions
+
+## Scope
+
+## Metamethods
+
+## Environments
+
+## Modules
+
+## Object Orientation
+
+## Patterns
+
+## Coroutines
+
+## Iterators
+
+## Garbage Collection
+
+## Weak Tables
+
+## Lua Standard Libraries
+
+### Core Functions
+	
+### Math Library
+	
+### String Library
+	
+### Table Library
+	
+### OS Library
+	
+### IO Library
+	
+### Module Library
+	
+### Debug Library
+	
+### Coroutine Library
+	
+## For
+
+## Advanced Concepts
+
+### Threads
+	
+### Optimisation
+	
+### Inheritance
